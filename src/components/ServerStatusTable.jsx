0992fdc51de9f2aef7a5030b6ce5d9d1142d4944
abhaysingh233 <136803392+abhaysingh233@users.noreply.github.com>
2026-01-05 20:22:02 +0530

    Delete src directory

--- src/components/ServerStatusTable.jsx
+++
@@ -1,8 +0,0 @@
-export default function ServerStatusTable({data}){
-return <table className="w-full bg-white shadow rounded">
-<thead><tr><th>IP</th><th>Status</th></tr></thead>
-<tbody>{data.map((s,i)=>(
-<tr key={i}><td>{s.ip}</td><td className={s.status==="UP"?"text-green-600":"text-red-600"}>{s.status}</td></tr>
-))}</tbody>
-</table>;
-}
--- src/components/TablespaceTable.jsx
+++
@@ -1,8 +0,0 @@
-export default function TablespaceTable({data}){
-return <table className="w-full bg-white shadow rounded">
-<thead><tr><th>Name</th><th>Used %</th></tr></thead>
-<tbody>{data.map((t,i)=>(
-<tr key={i}><td>{t.name}</td><td>{t.used}%</td></tr>
-))}</tbody>
-</table>;
-}
